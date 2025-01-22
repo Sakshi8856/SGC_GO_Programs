310e942c785c1209bb489292165fc2d1cf5cda0a
Sakshi Chavan <sc0682981@gmail.com>
2025-01-22 08:31:26 -0800

    added basic

---
+++ chapter1/2basic.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var student1 string = "tushar" //type is string
+	var student2 = "atharva"       //type is inferred
+	x := 2
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
+}
--- chapter2/return3.go
+++ chapter2/return3.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// Example of a simple for loop
+	
 	fmt.Println("Using a basic for loop:")
 	for i := 1; i <= 5; i++ {
 		fmt.Println(i)
 	}
 
-	// Example of an infinite loop (break when a condition is met)
+
 	fmt.Println("\nUsing a loop with break:")
 	count := 1
 	for {
@@ -20,11 +20,10 @@ func main() {
 		count++
 	}
 
-	// Example of a for loop with continue (skip even numbers)
 	fmt.Println("\nUsing continue to skip even numbers:")
 	for i := 1; i <= 10; i++ {
 		if i%2 == 0 {
-			continue // Skip even numbers
+			continue 
 		}
 		fmt.Println(i)
 	}
